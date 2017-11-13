9c7dcf57a6ba160aa0c7a775c26b3299d071fc3e
Fly J Gore <jennifer@theothergore.com>
2017-11-13 18:00:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+// this is fly j gore's first comment. Below is my solution for this lab.
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
