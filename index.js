fbb7a8b784fac3cfd5dfb1776769d2e480409c6b
Fly J Gore <jennifer@theothergore.com>
2017-11-13 17:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // this is fly j gore's first comment. Below is my solution for this lab.
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = "Chelsea";
-const companyCEO = "Susan Smith";
+let companyCEO = "Susan Smith";
